8aa27da5c33b41343344a93b919a5b39e1ba4e02
SeymurNaim <naimbeyov@mail.ru>
2024-07-01 22:36:45 +0400

    Creating app

---
+++ URLSessionPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  URLSessionPractice
+//
+//  Created by Apple on 01.07.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
